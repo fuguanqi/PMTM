0f48b8c80d8c9445a04da9003a586f1e5c41ced6
fuguanqi <fuguanqi@hotmail.com>
2023-09-04 20:26:08 +0800

    debug

--- test.m
+++ test.m
@@ -12,14 +12,15 @@ for i=1:numel(mac_array)
         for p=1:numel(mt_array)
             mac=mac_array(i);
             d=d_array(k);
-            mt=mt_array(P);
+            mt=mt_array(p);
+            data=importdata (strcat('INSTANCES\m',num2str(mac),'_j',num2str(j), ...
+                '_d',num2str(d),'_mt',num2str(mt),'_r',num2str(r),'.txt'));
             n=data(2);
             prob.n=n;
             prob.d=d;
             prob.mt=mt;
             prob.r=r;
-            data=importdata (strcat('INSTANCES\m',num2str(mac),'_j',num2str(j), ...
-                '_d',num2str(d),'_mt',num2str(mt),'_r',num2str(r),'.txt'));
+            
             prob.mac=mac;
             prob.p_i=data(3:n+2);
             prob.mt_j=data(n+3:n+2+mac);
