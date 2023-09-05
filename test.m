6f3a7b4010d322f42dab47e1f9d0b399df8f8f11
fuguanqi <fuguanqi@hotmail.com>
2023-09-05 15:46:02 +0800

    single proc

--- test.m
+++ test.m
@@ -7,27 +7,28 @@ mt_array=[3,9];
 
 r=0;
 j=20;
-for i=1:numel(mac_array)
-    for k=1:numel(d_array)
-        for p=1:numel(mt_array)
-            mac=mac_array(i);
-            d=d_array(k);
-            mt=mt_array(p);
-            data=importdata (strcat('INSTANCES\m',num2str(mac),'_j',num2str(j), ...
-                '_d',num2str(d),'_mt',num2str(mt),'_r',num2str(r),'.txt'));
-            n=data(2);
-            prob.n=n;
-            prob.d=d;
-            prob.mt=mt;
-            prob.r=r;
+for aa=1:8
+    for i=1:numel(mac_array)
+        for k=1:numel(d_array)
+            for p=1:numel(mt_array)
+                mac=mac_array(i);
+                d=d_array(k);
+                mt=mt_array(p);
+                data=importdata (strcat('INSTANCES\m',num2str(mac),'_j',num2str(j), ...
+                    '_d',num2str(d),'_mt',num2str(mt),'_r',num2str(r),'.txt'));
+                n=data(2);
+                prob.n=n;
+                prob.d=d;
+                prob.mt=mt;
+                prob.r=r;
 
-            prob.mac=mac;
-            prob.p_i=data(3:n+2);
-            prob.mt_j=data(n+3:n+2+mac);
-            prob.deter=data(n+3+mac:end);
-            prob.deter=reshape(prob.deter,[mac,n]);
+                prob.mac=mac;
+                prob.p_i=data(3:n+2);
+                prob.mt_j=data(n+3:n+2+mac);
+                prob.deter=data(n+3+mac:end);
+                prob.deter=reshape(prob.deter,[mac,n]);
 
-            parfor aa=1:8
+
                 SODAADM_DP(prob);
             end
 
